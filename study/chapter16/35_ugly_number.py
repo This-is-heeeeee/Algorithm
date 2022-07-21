362f8e49e0b8e5bf8186abb7abe8adebd52fafae
This-is-heeeeee <lkh951212@gmail.com>
2022-07-21 20:27:16 +0900

    chapter16

--- study/chapter16/35_ugly_number.py
+++ study/chapter16/35_ugly_number.py
@@ -1,1 +1,30 @@
 # 못생긴 수
+# 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, ...
+
+import pytest
+
+
+@pytest.mark.parametrize("n, expected",
+                         [(11, 15),
+                          (10, 12),
+                          (4, 4)])
+
+
+def test(n, expected):
+    answer = solution(n)
+    assert answer == expected
+
+
+def solution(n):
+    ugly_nums = [1]
+    num = 2
+    while len(ugly_nums) != n:
+        for ugly_num in ugly_nums:
+            if ugly_num * 2 == num or ugly_num * 3 == num or ugly_num * 5 == num:
+                ugly_nums.append(num)
+                break
+
+        num += 1
+
+    result = ugly_nums[-1]
+    return result
--- study/chapter16/36_edit_distance.py
+++ study/chapter16/36_edit_distance.py
@@ -1,1 +1,33 @@
 # 편집거리
+
+import pytest
+
+
+@pytest.mark.parametrize("A, B, expected",
+                         [("cat", "cut", 1),
+                          ("sunday", "saturday", 3)])
+
+
+def test(A, B, expected):
+    answer = solution(A, B)
+    assert answer == expected
+
+
+def solution(A, B):
+    edit_distance = [[0] * (len(B)+1) for _ in range(len(A)+1)]
+
+    for i in range(1, len(A) + 1):
+        edit_distance[i][0] = i
+    for j in range(1, len(B) + 1):
+        edit_distance[0][j] = j
+
+    for i in range(1, len(A) + 1):
+        for j in range(1, len(B) + 1):
+            if A[i-1] == B[j-1]:
+                edit_distance[i][j] = edit_distance[i-1][j-1]
+            else:
+                edit_distance[i][j] = min(edit_distance[i-1][j] + 1, edit_distance[i][j-1] + 1, edit_distance[i-1][j-1] + 1)
+
+    result = edit_distance[-1][-1]
+
+    return result
